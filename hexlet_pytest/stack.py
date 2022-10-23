4b652b62bd721dac9e6358380c06ed6a9eacd49f
Евгения Ольшанова <olshanova1995@gmail.com>
2022-10-23 10:26:03 +0300

    Залила второй тест stack

---
+++ hexlet_pytest/stack.py
@@ -0,0 +1,11 @@
+stack = []  # В питоне стек реализован через список
+not stack  # Список пуст
+stack.append(1)  # [1]
+stack.append(2)  # [1, 2]
+stack.append(3)  # [1, 2, 3]
+not stack  # Список не пустой
+stack # [1, 2, 3]
+stack.pop()  # В стеке [1, 2]
+stack.pop()  # В стеке [1]
+stack.pop()  # В стеке пусто
+not stack
